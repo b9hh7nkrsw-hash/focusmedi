45cc8585139a8f8bad1cab6edc08b434aa2d4dd0
b9hh7nkrsw-hash <b9hh7nkrsw@privaterelay.appleid.com>
2026-09-04 01:04:11 +0530

    fix(android): use configureFlutterEngine with non-null binaryMessenger; create asset folders to satisfy pubspec.yaml

--- android/app/src/main/kotlin/com/focusmedi/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/focusmedi/app/MainActivity.kt
@@ -8,14 +8,16 @@ import android.app.AppOpsManager
 import android.content.Context
 import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.focusmedi.app/platform"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             when (call.method) {
                 "startProtection" -> {
                     val modeId = (call.argument<Number>("modeId") ?: 0).toInt()
